e022432ecd72501a18c82da5e05ef248e78440bc
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-10-17 00:36:56 +0300

    Average of two numbers with function

---
+++ Ave.java
@@ -0,0 +1,12 @@
+public class Ave{
+	public static int calculate(int a, int b){
+	int sum = 0;
+	sum = (a + b) / 2;
+	System.out.println(sum);
+return sum;
+}
+	public static void main(String[] args){
+	System.out.print("Average of 4 and 6 is: ");
+	calculate(4,6);
+	}
+}
