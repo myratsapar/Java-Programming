4346d4522e8099b8d57d1932836aa6eea4568aa2
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-11-01 02:39:14 +0300

    First Scanner method. Expects input from user

--- ScannerMethod.java
+++ ScannerMethod.java
@@ -4,11 +4,11 @@ public class ScannerMethod{
 	public static void main(String[] args){
 		String name;
 		int age;
-Scanner input = new Scanner(System.in);
-System.out.printf("Enter name: ");
-name = input.nextLine();
-System.out.printf("Enter age: ");
-age = input.nextInt();
-System.out.printf("Hi %s, you are %d\n", name, age);
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter name: ");
+		name = input.nextLine();
+		System.out.print("Enter age: ");
+		age = input.nextInt();
+		System.out.printf("Hi %s, you are %d\n", name, age);
 	}
-}
+} //2:36 AM 01-Nov-17
