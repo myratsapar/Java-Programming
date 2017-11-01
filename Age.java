321a0d149e74233b2b35a9441cb1212ee2185e0e
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-11-01 15:38:22 +0300

    Ice Age

--- Age.java
+++ Age.java
@@ -9,23 +9,23 @@ int age;
 System.out.print("Enter age: ");
 age = input.nextInt();
 
-if(0 <= age && age < 13)
+	if(0 <= age && age < 13)
 System.out.print("Child status");
-else if (13 <= age && age < 25)
+	else if (13 <= age && age < 25)
 System.out.print("Teen/Adult status");
-else if (25 <= age && age < 37)
+	else if (25 <= age && age < 37)
 System.out.print("Middle-aged status");
-else if (37 <= age && age < 49)
+	else if (37 <= age && age < 49)
 System.out.print("Senior status");
-else if (49 <= age && age < 61)
+	else if (49 <= age && age < 61)
 System.out.print("Elderly status");
-else if (61 <= age && age < 73)
+	else if (61 <= age && age < 73)
 System.out.print("Old age status");
-else if (73 <= age && age < 85)
+	else if (73 <= age && age < 85)
 System.out.print("Kung Fu Masterly status");
-else if (85 <= age && age < 95)
+	else if (85 <= age && age < 95)
 System.out.print("Wizardly status");
-else 
+	else 
 System.out.print("Oghuzkhan status");
 
 	}
