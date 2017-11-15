d7da2b3240b4cedae4fb629b4f95645dad67a9fa
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-11-15 11:54:13 +0300

    Calculate sum with methods

---
+++ Sum.java
@@ -0,0 +1,16 @@
+class Sum{
+   int m, n, sum;
+
+public Sum(int a, int b){
+   m = a;
+   n = b;
+   sum = m + n;
+   System.out.println(sum);
+};
+
+public static void main(String[] args){
+
+Sum ss = new Sum(6, 9);
+
+  }
+}//12:39 AM 15-Nov-17
