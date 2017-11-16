8b5ebf9678ef53522131386bb8d5a27e91effcb5
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-11-16 22:31:26 +0300

    Fibonacci series

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+class Fibonacci{
+
+public static void main(String[] args){
+
+int n1 = 0, n2 = 1, n3, count = 10;
+System.out.print(n1 + " " + n2);
+
+for(int i = 2; i < count; i++){
+n3 = n2 + n1;
+System.out.print(" " + n3);
+n1 = n2;
+n2 = n3;
+  }
+ }
+}//11:11 AM 15-Nov-17
