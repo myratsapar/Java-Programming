18b85f759f4eade996edb44a7c714a1aa38b4fdb
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-11-16 22:30:58 +0300

    Print array elements like matrix

---
+++ Arrays1.java
@@ -0,0 +1,17 @@
+public class Arrays1{
+public static void main(String[] args){
+
+int [][] arr = {
+{11,22,35,46}, 
+{53,65,67,68},
+{99,10,11,12}
+};
+
+for (int i = 0; i < arr.length; i++){
+   for (int j = 0; j < arr[i].length; j++){
+   System.out.print(" " + arr[i][j]);
+   }
+System.out.println("");
+  }
+ }
+}//10:36 AM 15-Nov-17
