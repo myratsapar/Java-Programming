b973afe22dffc15769c04d758a765a2435346a44
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-11-16 22:30:18 +0300

    Arrays print array elements

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+public class Arrays{
+public static void main(String[] args){
+
+int [][] arr = new int [][] {{1,2,3}, {4,5,6}};
+
+for (int i = 0; i < arr.length; i++){
+   for (int j = 0; j < arr[i].length; j++){
+   System.out.print(" " + arr[i][j]);
+   }
+System.out.println("");
+  }
+ }
+}//10:33 AM 15-Nov-17
