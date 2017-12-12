82dc32b19d3b0878116870049b7f1c87c506da26
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-12-13 01:03:28 +0300

    Constructors: Constructor does all

---
+++ SumF.java
@@ -0,0 +1,13 @@
+//Constructor do all
+public class SumF {
+	int m, n, sum;
+	public SumF(int a, int b){
+		m = a;
+		n = b;
+		sum = m + n;
+		System.out.println(sum);
+	}
+public static void main(String[] args){
+	SumF sF = new SumF(9, 11);
+	}
+}//December 13, 2017 1:02:04 AM
