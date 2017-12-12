4cb2cc466c631738522d819ad76a449f738744db
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-12-13 00:44:04 +0300

    Constructors: Constructor without parameters

--- SumB.java
+++ SumB.java
@@ -1,7 +1,7 @@
 //Constructor without parameters
 public class SumB {
 int n, m, sum;
-public SumB(){ //constructor without parameters
+public SumB(){ //constructor
 	};
 	public static void main(String [] args){
 		SumB sB = new SumB();
