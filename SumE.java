228656118ab870c9e05db0a10f9dc621cbf6f608
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-12-13 00:58:55 +0300

    Constructors: Constructor as method

---
+++ SumE.java
@@ -0,0 +1,15 @@
+//Constructor as methods
+public class SumE {
+	int m, n;
+	static int sum;
+public SumE(int a, int b){
+		m = a;
+		n = b;
+		sum = m + n;
+	};
+
+public static void main(String[] args){
+	SumE sE = new SumE(19, 91);
+	System.out.println(sum);
+	}
+}//December 13, 2017 12:56:59 AM
