8c28e34e67ff43a332f4088153453fda87d707b3
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-12-13 00:35:21 +0300

    Constructor: Constructor without parameters

---
+++ SumB.java
@@ -0,0 +1,13 @@
+//Constructor without parameters
+public class SumB {
+int n, m, sum;
+public SumB(){ //constructor without parameters
+	};
+	public static void main(String [] args){
+		SumB sB = new SumB();
+		sB.m = 7;
+		sB.n = 15;
+		sB.sum = sB.m + sB.n;
+		System.out.println(sB.sum);
+	}
+}//December 13, 2017 12:32:15 AM
