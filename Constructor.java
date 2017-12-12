210ba5844330cd68f669f76457f0cc25aa4f93b0
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-12-13 00:25:23 +0300

    Constructors: Default Constructor

---
+++ Constructor.java
@@ -0,0 +1,10 @@
+public class Constructor {
+ int m, n, sum;
+ 	public static void main(String[] args){
+ 		Constructor c = new Constructor();
+ 		c.m = 3;
+ 		c.n = 5;
+ 		c.sum = c.m + c.n;
+ 		System.out.println(c.sum);
+ 	}
+}//December 13, 2017 12:20:37 AM
