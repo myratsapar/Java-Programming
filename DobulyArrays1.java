73d9def8f87de37b5a11cb86eba4c4fc1eb695fa
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-12-23 01:03:06 +0300

    Adds random items to doubly array

---
+++ DobulyArrays1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class DobulyArrays1 {
+	public static void main(String args[]){
+		int arr [][] = new int [4][5];
+		Random rand = new Random();
+			for(int i = 0; i < arr.length; i++){
+				for (int j = 0; j < arr[i].length; j++){
+					arr[i][j] = rand.nextInt(50);
+					System.out.print(arr[i][j]);
+					System.out.print("\t");
+				}
+				System.out.println();
+			}
+	}
+}//December 23, 2017 1:01:47 AM
