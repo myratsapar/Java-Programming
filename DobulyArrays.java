b7a8debabdca508b3277a9cc5a19238e8674503c
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2017-12-23 21:36:20 +0300

    Adds runtime variables to doubly arrays

--- DobulyArrays.java
+++ DobulyArrays.java
@@ -1,14 +1,13 @@
 public class DobulyArrays {
-	
 	public static void main(String args[]){
-		int arr [][] = new int [4][5];
-			for(int i = 0; i < arr.length; i++){
-				for (int j = 0; j < arr[i].length; j++){
-					arr[i][j] = i*j;
-					System.out.print(arr[i][j]);
-					System.out.print("\t");
-				}
-				System.out.println();
+	int arr [][] = new int [4][5];
+		for(int i = 0; i < arr.length; i++){
+			for (int j = 0; j < arr[i].length; j++){
+				arr[i][j] = i*j;
+				System.out.print(arr[i][j]);
+				System.out.print("\t");
 			}
+			System.out.println();
+		}
 	}
 }//December 23, 2017 12:55:29 AM
